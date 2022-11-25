ce1525b7da88f8042ac25a6f5cbf1438b4a09c25
yusupovay <yusupovay625@gmail.com>
2022-11-25 17:34:02 +0500

    text

---
+++ main.js
@@ -0,0 +1,8 @@
+//
+//
+var userGener = prompt("male" + "\n" + "female" + "\n");
+var userName = prompt("Abdullo" + "\n" + "Madina");
+if (male == janob) {
+  console.log("Janob" + "");
+} else {
+}
